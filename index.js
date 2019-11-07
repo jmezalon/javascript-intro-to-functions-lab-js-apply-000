1cb5a3021e8351bb7f933b27a70d087ea695c978
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-07 15:05:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ function logWhisper(string) {
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase === lowercase) {
-    return "I can\t hear you!"
+    return "I can\'t hear you!"
   } 
+}
+
+function sayHiToGrandma (string) {
+  if (string.toUpperCase === uppercase) {
+    return "YES INDEED"
+  }
+}
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
+  }
 }
