8c6c760c7e060d20a2ef76250c067580dd2bf712
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-07 15:23:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ function logWhisper(string) {
 function sayHiToGrandma (string) {
   // let output = "I can\'t hear you";
   if (string.toLowerCase === lowercase) {
-    console.log("I can\'t hear you")
+    console.log("I can\'t hear you!")
   } 
 }
 
 function sayHiToGrandma (string) {
   if (string.toUpperCase === uppercase) {
-    console.log("YES INDEED")
+    return "YES INDEED!";
   }
 }
 
 function sayHiToGrandma(string) {
   // let output= "I love you, too."
   if (whisper(string)) {
-    console.log('I love you, too')
+    console.log('I love you, too.')
   }
 }
